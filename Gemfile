98611fcb38670b20a938893064166d32e9f6feec
Karim <inkspand@gmail.com>
2013-07-16 19:06:13 -0400

    Chapter 4 Start

--- Gemfile
+++ Gemfile
@@ -5,9 +5,10 @@ gem 'rails', '3.2.3'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-group :development, :test do
+group :development do
 	gem 'rspec-rails', '2.9.0'
 	gem 'sqlite3', '1.3.5'
+	gem 'guard-rspec', '0.5.5'
 end
 
 gem 'json'
@@ -29,7 +30,13 @@ end
 gem 'jquery-rails'
 
 group :test do
+	gem 'rspec-rails', '2.9.0'
 	gem 'capybara', '1.1.2'
+	gem 'guard-spork', '0.3.2'
+	gem 'spork', '0.9.0'
+# Test gems on Mac OSX (p121 of tutorial)
+	gem 'rb-fsevent', '0.4.3.1', :require => false
+	gem 'growl', '1.0.3'
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Static Pages" do
-	let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
 
@@ -14,10 +14,15 @@ describe "Static Pages" do
       # response.status.should be(200)
 	  end
 
-	  it "should have the title 'Home'" do
+	  it "should have the base title" do
 	  	visit '/static_pages/home'
 	  	page.should have_selector('title',
-	  		:text => "#{base_title} | Home")
+	  		:text => "Ruby on Rails Tutorial Sample App")
+	  end
+
+	  it "should not have a custom page title" do
+	  	visit '/static_pages/home'
+	  	page.should_not have_selector('title', :text => '| Home')
 	  end
 	end
 	
