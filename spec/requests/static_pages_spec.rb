6893c7d404d5ba87a136a0719a735eba0ccb50c8
Karim <inkspand@gmail.com>
2013-07-16 13:07:25 -0400

    Postegresql change

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+	let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
 
     it "should have the h1 'Sample App'" do
 		visit '/static_pages/home'
 		page.should have_selector('h1',
-			:text => "Sample App")
+			:text => 'Sample App')
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       # get static_pages_index_path
       # response.status.should be(200)
@@ -15,7 +17,7 @@ describe "Static Pages" do
 	  it "should have the title 'Home'" do
 	  	visit '/static_pages/home'
 	  	page.should have_selector('title',
-	  		:text => "Ruby on Rails Tutorial Sample App | Home")
+	  		:text => "#{base_title} | Home")
 	  end
 	end
 	
@@ -29,7 +31,7 @@ describe "Static Pages" do
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
 			page.should have_selector('title', 
-				:text => "Ruby on Rails Tutorial Sample App | Help")
+				:text => "#{base_title} | Help")
 		end
 	end
 	
@@ -43,8 +45,21 @@ describe "Static Pages" do
 		it "should have the title 'About Us'" do
 			visit '/static_pages/about'
 			page.should have_selector('title', 
-				:text => "Ruby on Rails Tutorial Sample App | About Us")
+				:text => "#{base_title} | About Us")
 		end
-    end
-  
+	end
+	
+	describe "Contact page" do
+
+		it "should have the h1 'Contact Us'" do
+		visit '/static_pages/contact'
+		page.should have_selector('h1', :text => 'Contact Us')
+		end
+
+		it "should have the title 'Contact Us'" do
+			visit '/static_pages/contact'
+			page.should have_selector('title', 
+				:text => "#{base_title} | Contact Us")
+		end
+	end
 end
